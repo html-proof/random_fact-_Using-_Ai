ec51c90072f3138ac283b8be6d3284be581b3265
Sebastian <68529971+html-proof@users.noreply.github.com>
2022-10-10 23:12:40 +0530

    Ai pro to speak about randomfacts

---
+++ facts_speaker.py
@@ -0,0 +1,16 @@
+import pyttsx3
+from time import  sleep
+import randfacts
+
+engine =pyttsx3.init()
+try:
+    for i in range(10):
+        x = randfacts.get_fact(True)
+        print(x)
+        engine.say(x)
+        engine.runAndWait()
+        sleep(2)
+except DeprecationWarning:
+    pass
+except KeyboardInterrupt:
+    pass
